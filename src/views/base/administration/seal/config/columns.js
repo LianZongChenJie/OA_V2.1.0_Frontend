83999d31e40e987b685c055703994e37c515734f
wangjian <wangjian@company.com>
2026-04-01 13:43:09 +0800

    更改图标

--- src/views/base/administration/seal/config/columns.js
+++ src/views/base/administration/seal/config/columns.js
@@ -81,7 +81,7 @@ export const getHeaderButs = (onAdd) => [
   { label: '新增', type: 'primary', icon: 'plus', size: 'default', onClick: onAdd },
 ];
 
-export const getOperationColumn = (onEdit, onDisable, onView) => {
+export const getOperationColumn = (onEdit, onDisable, onenable) => {
   return {
     label: '操作',
     width: 200,
@@ -112,9 +112,9 @@ export const getOperationColumn = (onEdit, onDisable, onView) => {
         type: 'primary',
         size: 'small',
         onClick: (row) => {
-          onDisable && onDisable(row);
+          onenable && onenable(row);
         },
-        icon: 'unlock',
+        icon: 'enable',
         isShow: (row) => row.status === 0,
       },
     ],
