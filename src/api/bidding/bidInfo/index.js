4330d3d39d65ae77f09725bb4c9587b8dc0a561b
李强 <liqiang@omsedu.com>
2026-05-07 10:32:05 +0800

    修复消息模版BUG

---
+++ src/api/bidding/bidInfo/index.js
@@ -0,0 +1,65 @@
+import request from '@/utils/request'
+
+// 招投标信息分页列表
+export function getPageList(query) {
+  return request({
+    url: '/tender/list',
+    method: 'get',
+    params: query
+  })
+}
+// 招投标信息详情
+export function getDetail(id) {
+  return request({
+    url: `/tender/${id}`,
+    method: 'get'
+  })
+}
+// 招投标信息新增
+export function add(data) {
+  return request({
+    url: '/tenderadd',
+    method: 'post',
+    data
+  })
+}
+// 招投标信息修改
+export function edit(data) {
+  return request({
+    url: '/tenderupd',
+    method: 'put',
+    data
+  })
+}
+// 删除招投标信息
+export function del(id) {
+  return request({
+    url: `/tender/${id}`,
+    method: 'delete'
+  })
+}
+
+// 招投标信息倒入模版下载
+export function downloadTmp(id) {
+  return request({
+    url: `/tender/import/template`,
+    method: 'get'
+  })
+}
+// 招投标信息导入
+export function importData(data) {
+  return request({
+    url: `/tender/import`,
+    method: 'post',
+    data
+  })
+}
+// 招投标信息导出
+export function exportData(query) {
+  return request({
+    url: `/tender/export`,
+    method: 'get',
+    params: query
+  })
+}
+//
---
+++ src/views/bidding/bidInfo/config/colums.js
@@ -0,0 +1,162 @@
+export const columns = [
+  {
+    fieldName: 'id',
+    label: 'ID号',
+    width: 80,
+    minWidth: 80,
+    align: 'center',
+  },
+  {
+    fieldName: 'checkStatus',
+    label: '审核状态',
+    width: 100,
+    minWidth: 100,
+    align: 'center',
+    isDict: true,
+    dict: 'check_status',
+  },
+  {
+    fieldName: 'title',
+    label: '公文主题',
+    minWidth: 200,
+    searchable: {
+      type: 'input',
+      fieldName: 'keywords',
+      placeholder: '请输入关键字',
+      label: '关键字',
+      order: 0,
+    },
+  },
+  {
+    fieldName: 'code',
+    label: '公文编号',
+    width: 150,
+    minWidth: 120,
+  },
+  {
+    fieldName: 'secrets',
+    label: '密级程度',
+    width: 100,
+    minWidth: 100,
+    align: 'center',
+    isDict: true,
+    dict: 'secrets_level',
+    searchable: {
+      type: 'select',
+      dictKey: 'secrets_level',
+      fieldName: 'secrets',
+      placeholder: '请选择密级程度',
+      label: '密级程度',
+      order: 1,
+    },
+  },
+  {
+    fieldName: 'urgency',
+    label: '紧急程度',
+    width: 100,
+    minWidth: 100,
+    align: 'center',
+    isDict: true,
+    dict: 'urgency_level',
+    searchable: {
+      type: 'select',
+      dictKey: 'urgency_level',
+      fieldName: 'urgency',
+      placeholder: '请选择紧急程度',
+      label: '紧急程度',
+      order: 2,
+    },
+  },
+  {
+    fieldName: 'draftName',
+    label: '拟稿人',
+    width: 100,
+    minWidth: 80,
+  },
+  {
+    fieldName: 'draftTime',
+    label: '拟稿日期',
+    width: 160,
+    minWidth: 140,
+  },
+  {
+    fieldName: 'draftDname',
+    label: '拟稿部门',
+    width: 120,
+    minWidth: 100,
+  },
+  {
+    fieldName: 'createTime',
+    label: '创建时间',
+    width: 180,
+    minWidth: 160,
+  },
+];
+
+export const operationColumn = {
+  label: '操作',
+  width: 180,
+  fixed: 'right',
+  show: true,
+  actions: [
+    {
+      label: '编辑',
+      type: 'success',
+      size: 'small',
+      onClick: (row, onEdit) => {
+        onEdit && onEdit(row);
+      },
+      icon: 'edit',
+    },
+  ],
+};
+
+export const getHeaderButs = (onAdd) => [
+  { label: '新增', type: 'primary', icon: 'plus', size: 'default', onClick: onAdd },
+];
+
+export const getOperationColumn = (onEdit, onView, onDelete) => {
+  return {
+    label: '操作',
+    width: 240,
+    fixed: 'right',
+    show: true,
+    actions: [
+      {
+        label: '编辑',
+        type: 'success',
+        size: 'small',
+        isShow: (row) => Number(row.checkStatus) === 2,
+        onClick: (row) => {
+          onEdit && onEdit(row);
+        },
+        icon: 'edit',
+      },
+      {
+        label: '详情',
+        type: 'primary',
+        size: 'small',
+        onClick: (row) => {
+          onView && onView(row);
+        },
+        icon: 'eye-open',
+      },
+      {
+        label: '删除',
+        type: 'danger',
+        size: 'small',
+        isShow: (row) => Number(row.checkStatus) === 2,
+        onClick: (row) => {
+          onDelete && onDelete(row);
+        },
+        icon: 'delete',
+      },
+    ],
+  };
+};
+
+export default {
+  columns,
+  operationColumn,
+  getHeaderButs
+};
